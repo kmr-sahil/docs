d8784b6397f2066a2161a996c5a4113aef17f005
Sahil <sk2003311@gmail.com>
2023-12-30 13:36:21 +0530

    framer motion done

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-function Card({data}) {
+function Card({data, refrence}) {
     
   return (
-    <div className='relative w-60 h-72 rounded-[30px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <motion.div drag 
+        dragConstraints={refrence} 
+        whileDrag={{scale: 1.1}} 
+        dragElastic={0.2} 
+        dragTransition={{bounceStiffness: 100, bounceDamping:30}}
+        className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+
         <FaRegFileAlt />
         <p className='text-sm leading-tight mt-5 font-thin'>{data.desc}</p>
         <div className='footer absolute bottom-0 w-full left-0'>
@@ -18,12 +25,16 @@ function Card({data}) {
                 </span>
                 
             </div>
-
-            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-                <h3 className='text-sm'>Download Now</h3>
-            </div>
+            {
+                data.tag.isOpen && (
+                <div className={`tag w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+                    <h3 className='text-sm'>{data.tag.tagTitle}</h3>
+                </div>
+                )
+            }
+            
         </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/componets/Foreground.jsx
+++ src/componets/Foreground.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
+import { useRef } from 'react';
 import Card from './Card'
 
 function Foreground() {
+
+    const ref = useRef(null);
 
     const data = [{
         desc: "nhsgjsdgfhsjs",
         filesize: ".9mb",
         close: true,
         tag: {
+            isOpen: false,
+            tagTitle: "Download Now",
+            tagColor: "blue"
+        }
+    },
+    {
+        desc: "nhsgjsdgfhsjs",
+        filesize: ".9mb",
+        close: false,
+        tag: {
             isOpen: true,
             tagTitle: "Download Now",
-            tagColor: "Green"
+            tagColor: "blue"
+        }
+    },
+    {
+        desc: "nhsgjsdgfhsjs",
+        filesize: ".9mb",
+        close: true,
+        tag: {
+            isOpen: true,
+            tagTitle: "Download Now",
+            tagColor: "green"
         }
     }
     ];
 
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
         {data.map((item, index) => (
-            <Card data={item}/>
+            <Card data={item} refrence={ref}/>
         ))}
     </div>
   )
