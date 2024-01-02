bf3a4bd405ea6018aa0f1f800f27906155dbcdd6
Sahil <sk2003311@gmail.com>
2024-01-02 22:02:18 +0530

    added user input form

--- src/componets/Foreground.jsx
+++ src/componets/Foreground.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRef } from 'react';
 import Card from './Card'
 
@@ -6,44 +6,74 @@ function Foreground() {
 
     const ref = useRef(null);
 
-    const data = [{
-        desc: "nhsgjsdgfhsjs",
-        filesize: ".9mb",
-        close: true,
-        tag: {
-            isOpen: false,
-            tagTitle: "Download Now",
-            tagColor: "blue"
+    const [cardDetails, setCardDetails] = useState(
+        [{
+            desc: "nhsgjsdgfhsjs",
+            filesize: ".9mb",
+            close: true,
+            tag: {
+                isOpen: false,
+                tagTitle: "Download Now",
+                tagColor: "blue"
+            }
+        },
+        {
+            desc: "nhsgjsdgfhsjs",
+            filesize: ".9mb",
+            close: false,
+            tag: {
+                isOpen: true,
+                tagTitle: "Download Now",
+                tagColor: "blue"
+            }
+        },
+        {
+            desc: "nhsgjsdgfhsjs",
+            filesize: ".9mb",
+            close: true,
+            tag: {
+                isOpen: true,
+                tagTitle: "Download Now",
+                tagColor: "green"
+            }
         }
-    },
-    {
-        desc: "nhsgjsdgfhsjs",
-        filesize: ".9mb",
-        close: false,
-        tag: {
-            isOpen: true,
-            tagTitle: "Download Now",
-            tagColor: "blue"
-        }
-    },
-    {
-        desc: "nhsgjsdgfhsjs",
-        filesize: ".9mb",
-        close: true,
-        tag: {
-            isOpen: true,
-            tagTitle: "Download Now",
-            tagColor: "green"
-        }
+        ]
+    );
+
+    const onSubmit = () => {
+        setCardDetails([...cardDetails,{
+            desc: "testing",
+            filesize: ".9mb",
+            close: true,
+            tag: {
+                isOpen: true,
+                tagTitle: "Download Now",
+                tagColor: "green"
+            }
+        }])
+        console.log(cardDetails)
     }
-    ];
 
   return (
-    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
-        {data.map((item, index) => (
-            <Card data={item} refrence={ref}/>
-        ))}
-    </div>
+    <>
+        <div className='w-[40%] mx-auto z-[9] flex justify-evenly p-[1rem] flex-wrap'>
+            <input className='w-[80%] rounded-md p-[0.5rem] outline-slate-600 outline-offset-4' placeholder='add files or todo'></input>
+            <select id="cars" name="cars">
+                    <option value="Checkbox">Volvo</option>
+                    <option value="Note">Saab</option>
+                    <option value="Link">Fiat</option>
+            </select>
+            <textarea name="message" rows="10">The cat was playing in the garden.</textarea>
+            <input placeholder='tagTitle' />
+            <button onClick={onSubmit} className='w-[10%] rounded-md bg-white'>Add</button>
+        </div>
+        <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5 mt-[4rem]'>
+            {cardDetails.map((item, index) => (
+                <Card data={item} refrence={ref}/>
+            ))}
+        </div>
+    </>
+   
   )
 }
 
