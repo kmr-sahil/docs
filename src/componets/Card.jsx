8c52eacc0d806802542211e30b4792b526340587
Sahil <sk2003311@gmail.com>
2024-01-04 16:26:00 +0530

    updated the cards

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -14,25 +14,18 @@ function Card({data, refrence}) {
         dragTransition={{bounceStiffness: 100, bounceDamping:30}}
         className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
 
-        <FaRegFileAlt />
-        <p className='text-sm leading-tight mt-5 font-thin'>{data.desc}</p>
+        
+        {data.type == "checkbox" ? (<input type="checkbox" />) : null}
+        <h1>{data?.title}</h1>
+        <p className='text-sm leading-tight mt-5 font-thin'>{data?.desc}</p>
         <div className='footer absolute bottom-0 w-full left-0'>
-            <div className=' flex items-center justify-between py-3 px-8 mb-3'>
-                <h5>{data.filesize}</h5>
-                <span className='w-5 h-5 bg-zinc-600 rounded-full flex items-center justify-center '>
-                    {data.close ? <IoClose/> : <LuDownload size=".7em" color='#fff'/>}
-                    
-                </span>
-                
-            </div>
-            {
-                data.tag.isOpen && (
-                <div className={`tag w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
-                    <h3 className='text-sm'>{data.tag.tagTitle}</h3>
-                </div>
-                )
-            }
             
+        <div className={`tag w-full py-4 ${data?.tagColor === "blue" ? "bg-blue-600" : `${data?.tagColor === "green" ? "bg-green-600" : "bg-red-600"}` } flex items-center justify-center`}>
+            {data.link != "" ? (<a href={data.link}>{data?.tagTitle}</a>) : 
+                (<h3 className='text-sm'>{data?.tagTitle}</h3>)}
+            
+        </div>
+       
         </div>
     </motion.div>
   )
--- src/componets/Foreground.jsx
+++ src/componets/Foreground.jsx
@@ -6,73 +6,93 @@ function Foreground() {
 
     const ref = useRef(null);
 
+    const [details, setDetails] = useState({
+            type: "",
+            title: "",
+            desc: "",
+            tagColor : "",
+            tagTitle: "",
+            link: "",
+    })
+
     const [cardDetails, setCardDetails] = useState(
         [{
+            type: "checkbox",
+            title: "Head me",
             desc: "nhsgjsdgfhsjs",
-            filesize: ".9mb",
-            close: true,
-            tag: {
-                isOpen: false,
-                tagTitle: "Download Now",
-                tagColor: "blue"
-            }
+            tagColor : "red",
+            tagTitle: "hyij",
+            link: "",
         },
         {
+            type: "note",
+            title: "Head me 2",
             desc: "nhsgjsdgfhsjs",
-            filesize: ".9mb",
-            close: false,
-            tag: {
-                isOpen: true,
-                tagTitle: "Download Now",
-                tagColor: "blue"
-            }
+            tagColor : "green",
+            tagTitle: "hyij",
+            link: "",
         },
         {
+            type: "link",
+            title: "Head me",
             desc: "nhsgjsdgfhsjs",
-            filesize: ".9mb",
-            close: true,
-            tag: {
-                isOpen: true,
-                tagTitle: "Download Now",
-                tagColor: "green"
-            }
-        }
+            tagColor : "blue",
+            tagTitle: "hyij",
+            link: "https://tailwindcss.com/docs/browser-support#vendor-prefixes",
+        },
         ]
     );
 
     const onSubmit = () => {
-        setCardDetails([...cardDetails,{
-            desc: "testing",
-            filesize: ".9mb",
-            close: true,
-            tag: {
-                isOpen: true,
-                tagTitle: "Download Now",
-                tagColor: "green"
-            }
-        }])
+        console.log("haha" + details)
+        setCardDetails([...cardDetails, details])
         console.log(cardDetails)
     }
 
   return (
     <>
 
-        <div className='w-[40%] mx-auto z-[30] fixed top-0 left-1/2 -translate-x-[50%]  flex justify-evenly gap-[1rem] p-[1rem] flex-wrap'>
+        <div className='w-[40%] mx-auto z-[30] fixed top-0 left-1/2 -translate-x-[50%] bg-slate-300 flex justify-evenly gap-[1rem] p-[1rem] flex-wrap'>
             
-            <input className='w-[80%] rounded-md p-[0.5rem] outline-slate-600 outline-offset-4' placeholder='add files or todo'></input>
+            <input className='w-[80%] rounded-md p-[0.5rem] outline-slate-600 outline-offset-4' 
+                    placeholder='add files or todo'
+                    value={details.title}
+                    onChange={(e) => setDetails({...details, title: e.target.value})}
+                    />
             <button onClick={onSubmit} className='w-[10%] rounded-md bg-white'>Add</button>
-            <label for="type">Choose a type:</label>
-            <select id="type" name="type">
-                    <option value="Checkbox">Checkbox</option>
-                    <option value="Note">Note</option>
-                    <option value="Link">Link</option>
-            </select>
-            <textarea name="message" rows="2">The cat was playing in the garden.</textarea>
-            <input placeholder='tagTitle' />
-            <select id="cars" name="cars">
-                    <option value="Checkbox">Checkbox</option>
-                    <option value="Note">Note</option>
-                    <option value="Link">Link</option>
+            
+            <label>
+                Pick a Type:
+                <select name="type" 
+                        defaultValue="checkbox"
+                        value={details.type}
+                        onChange={(e) => setDetails({...details, type: e.target.value})}>
+                  <option value="checkbox">Checkbox</option>
+                  <option value="note">Note</option>
+                  <option value="link">Link</option>
+                </select>
+            </label>
+            <textarea name="message" 
+                        rows="2" 
+                        placeholder='your description...'
+                        value={details.desc}
+                        onChange={(e) => setDetails({...details, desc: e.target.value})} 
+            />
+                        
+
+            <input placeholder='tagTitle'
+                   value={details.tagTitle}
+                   onChange={(e) => setDetails({...details, tagTitle: e.target.value})} 
+            />
+
+            <label htmlFor="color">Choose a tag Color: </label>
+            <select id="color" 
+                    name="color"
+                    value={details.tagColor}
+                    onChange={(e) => setDetails({...details, tagColor: e.target.value})}>
+                    <option value="Red">Red</option>
+                    <option value="Green">Green</option>
+                    <option value="Blue">Blue</option>
             </select>
             
         </div>
